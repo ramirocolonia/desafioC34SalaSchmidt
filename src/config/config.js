76f5bbc78b041a83667ba7c181394cd2a7d335da
Ramiro Sala <ramiro.sala85@gmail.com>
2024-05-07 16:31:41 -0300

    endpoint de test y customizacion de logs completa

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,12 @@
 import dotenv from "dotenv";
 
-dotenv.config({path: ".env.development"});
+dotenv.config({path: ".env"});
 
 export default {
   port: process.env.PORT,
   mongoURL : process.env.MONGO_URL,
   admin: process.env.ADMIN_NAME,
   passAdmin: process.env.ADMIN_PASSWORD,
-  tokenPass: process.env.TOKEN_PASSWORD
+  tokenPass: process.env.TOKEN_PASSWORD,
+  environment: process.env.ENVIRONMENT
 }
--- src/controllers/mocking.controller.js
+++ src/controllers/mocking.controller.js
@@ -9,6 +9,17 @@ class MockingController{
       res.send({ status: "error", message: "Error en ejecución de función generate products con faker, " + error });
     }
   };
+
+  loggerTest = (req, res) => {
+    req.logger.fatal("este es un msj de nivel FATAL");
+    req.logger.error("este es un msj de nivel ERROR");
+    req.logger.warning("este es un msj de nivel WARNING");
+    req.logger.info("este es un msj de nivel INFO");
+    req.logger.http("este es un msj de nivel HTTP");
+    req.logger.debug("este es un msj de nivel DEBUG");
+    
+    res.send("Logger implementado!")
+  };
 }
 
 export default MockingController;
